c5dd4902e54fcce5c485f34c5493343cbaef3f93
Lício Lima <liciomlima@gmail.com>
2022-03-24 10:26:25 -0300

    Atualização no bot

--- Bot do WhatsApp.py
+++ Bot do WhatsApp.py
@@ -1,4 +1,10 @@
 import pywhatkit
+from time import sleep
 
-pywhatkit.sendwhatmsg('+5581973431677', 'O bot do Carlitos que chegou para GERAR!', 9, 40)
+def send_msg (number: str, message: str, hour: int, min: int):
+    pywhatkit.sendwhatmsg(number, message, hour, min)
 
+send_msg('+5581973431677', 'ME DESBLOQUEIA, IGUAIRAAAAA', 10, 21)
+send_msg('+5581995254823', 'BORA TOMAR MINOXIDIL?', 10, 23)
+send_msg('+5581993289246', 'BORA VER METAL & CEGO?', 10, 25)
+send_msg('+5581997451067', 'PIQUINIQUE COM OS SAGUINS', 10, 27)
